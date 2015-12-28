2e02ceb39d4ef4265cb8b91c86673d4c1595f499
Emily Dela Cruz <delacruz.emily@gmail.com>
2015-12-27 19:18:47 -0500

    Update: theme setup

---
+++ custom/themes/makeslowhaste-vone/index.php
@@ -0,0 +1,27 @@
+<style>
+	body {
+		color: black;
+	}
+	h1 {
+		margin: 0;
+	}
+	p {
+		margin: 0;
+		padding-top: 0px;
+		font-family: sans-serif;
+		font-size: 11px;
+	}
+	.main-text {
+		margin: 25% 25%;
+	}
+	
+</style>
+
+<div class="main-text">
+	<h1 class="fl">
+		festina lente.
+	</h1>
+	<p>
+		coming soon.
+	</p>
+</div>
